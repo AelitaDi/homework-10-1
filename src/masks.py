7297db1b943aa7dfe40acd81f889143f6614b057
uliana rogova <uv.rogova@yandex.ru>
2024-07-01 22:38:05 +0300

    docstrings changed

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,10 @@
 def get_mask_card_number(card_number: str) -> str:
-    """ " Функция маскирует номер карты клиента"""
+    """ Function masking client card number."""
 
     return card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
 
 
 def get_mask_account(user_account: str) -> str:
-    """ " Функция маскирует номер счета клиента"""
+    """ Function masking user account."""
 
     return "**" + user_account[-4:]
