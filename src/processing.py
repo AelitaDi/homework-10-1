6fd8cc728164e51c5f5bde9d305dccea8ce14dfc
uliana rogova <uv.rogova@yandex.ru>
2024-07-01 23:41:49 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,3 +2,9 @@ def filter_by_state(operation_list: list[dict], state: str = "EXECUTED") -> list
     """ Function filter operations by state."""
 
     return [operation for operation in operation_list if operation["state"] == state]
+
+
+def sort_by_date(operation_list: list[dict], sort_flag: bool = True) -> list[dict]:
+    """ Function sort operations by date."""
+
+    return sorted(operation_list, key=lambda date: date["date"], reverse=sort_flag)
