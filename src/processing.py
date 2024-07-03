550ab4a35bc3346278ed33c23c6da7c6e95c88ec
uliana rogova <uv.rogova@yandex.ru>
2024-07-03 22:54:50 +0300

    add test_masks

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
 def filter_by_state(operation_list: list[dict], state: str = "EXECUTED") -> list[dict]:
-    """ Function filter operations by state."""
+    """Function filter operations by state."""
 
     return [operation for operation in operation_list if operation["state"] == state]
 
 
 def sort_by_date(operation_list: list[dict], sort_flag: bool = True) -> list[dict]:
-    """ Function sort operations by date."""
+    """Function sort operations by date."""
 
     return sorted(operation_list, key=lambda date: date["date"], reverse=sort_flag)
---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture()
+def no_number_1():
+    return "sfsdfsdf"
+
+
+@pytest.fixture()
+def no_number_2():
+    return "1231276"
+
+
+@pytest.fixture()
+def no_number_3():
+    return ""
+
+
+@pytest.fixture()
+def no_number_4():
+    return "1342343534654466575568768647586845763457643875629843756"
