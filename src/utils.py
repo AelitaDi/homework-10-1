db1790158394541b8ea9aef03e316aad93b56e35
uliana rogova <uv.rogova@yandex.ru>
2024-07-26 23:49:31 +0300

    add logging for src.utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
 import json
+import logging
 import os
 
 from src.external_api import get_currency_conversion_to_rubles
+
+logging.basicConfig(
+    filename=os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "utils.log"),
+    filemode="w",
+    format="%(asctime)s: %(name)s: %(levelname)s: %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger(__name__)
 
 PATH_TO_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
 
@@ -9,30 +19,33 @@ PATH_TO_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", 
 def get_transactions_from_json(path_to_file: str) -> list[dict]:
     """Function get transactions from JSON file."""
 
+    logger.info(f"Запуск функции {get_transactions_from_json.__name__}.")
     try:
         with open(path_to_file, encoding="utf-8") as file:
             data = json.load(file)
+        logger.info(f"Успешное завершение работы функции {get_transactions_from_json.__name__}")
         return data
-    except Exception:
+    except Exception as e:
+        logger.error(f"Ошибка. {e}.")
         return []
 
 
 def get_transaction_amount(transaction: dict) -> float:
     """Function get amount of transaction in Rubles."""
 
+    logger.info(f"Запуск функции {get_transaction_amount.__name__}.")
     try:
         if transaction["operationAmount"]["currency"]["code"] != "RUB":
+            logger.info(
+                f"Успешное завершение работы функции {get_transaction_amount.__name__}, "
+                f"обращение к функции {get_currency_conversion_to_rubles.__name__}."
+            )
             return get_currency_conversion_to_rubles(
                 transaction["operationAmount"]["currency"]["code"], float(transaction["operationAmount"]["amount"])
             )
         else:
+            logger.info(f"Успешное завершение работы функции {get_transaction_amount.__name__}.")
             return float(transaction["operationAmount"]["amount"])
-    except Exception:
-        print("Error operationAmount")
+    except Exception as e:
+        logger.error(f"Ошибка. {e}.")
         return 0.0
-
-
-# if __name__ == "__main__":
-#     trans = get_transactions_from_json(PATH_TO_FILE)
-#     for tr in trans:
-#         print(get_transaction_amount(tr))
